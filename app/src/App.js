b7719f10cdcccce969e5487971d11df0bf813ea2
Žan Žlender <a1zan.zlender@gmail.com>
2022-01-09 22:35:53 +0100

    PREZENTACIJA Added console logs for educational purposes
    Added console logs when and where a service gets and sends OAuth, and/or required data.

--- app/src/App.js
+++ app/src/App.js
@@ -60,6 +60,7 @@ function App() {
       if (apiResponse.status === 200) {
         const responseObject = await apiResponse.json();
         cookie.set("access_token", responseObject.access_token);
+        console.log("Dobio sam token: " + responseObject.access_token);
 
         //automaticlly check if our server can decrypt encrypted token
         const apiStatus = await fetch("http://localhost:3001/text", {
@@ -112,6 +113,7 @@ function App() {
 
     if (apiResponse.status === 200) {
       const response = await apiResponse.json();
+      console.log("Dobio sam URL: " + response.authURL);
       setURL(response.authURL);
       setOpen(true);
     } else {
